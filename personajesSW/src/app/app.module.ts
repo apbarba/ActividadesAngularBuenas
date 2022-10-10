7af2f99627c37dee34876e84a9bf840a44ef4566
Ana Pilar Barba López <pilarbarba03@gmail.com>
2022-10-10 13:30:11 +0200

    Arreglando el problemilla

--- personajesSW/src/app/app.module.ts
+++ personajesSW/src/app/app.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { PersonajesComponent } from './componentes/personajes/personajes.component';
 import { HttpClientModule } from '@angular/common/http';
 import { MaterialImportsModule } from './material-imports/material-imports.module';
 import { PersonajesFotosComponent } from './personajes-fotos/personajes-fotos.component';
+import { PersonajesComponent } from './componentes/personajes-lista/personajes.component';
 
 @NgModule({
   declarations: [
